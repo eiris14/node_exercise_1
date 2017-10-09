3ceab9e7202d5818d50757d2cb7d5520a86b0796
Eirik Steinsland <Eirik@Eiriks-MacBook-Pro-2.local>
2017-10-09 16:25:02 +0200

    First commit

---
+++ greeter.js
@@ -0,0 +1,11 @@
+var greetingNumb = parseInt(Math.random()*3);
+
+if (greetingNumb == 1){
+    console.log("Hello there!")
+}
+else if(greetingNumb == 2){
+    console.log("Nice to see you!")
+}
+else{
+    console.log("Greetings and salutations!")
+}
